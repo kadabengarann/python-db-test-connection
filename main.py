301dc41b74663a11a41f20cb48f09454d6d2f2c3
Ardhy Satrio <rio27dh@gmail.com>
2024-07-29 15:54:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,24 +7,26 @@ from langchain_community.utilities import SQLDatabase
 
 # Load environment variables from the .env file
 load_dotenv()
-
-# Get the database URI from the environment variables
-URI = os.getenv('DB_URI')
-DB_URI = URI
-
-engine = create_engine(DB_URI)
-
-connection = engine.connect()
-
-print("DB connection successful!!")
-
-inspector = inspect(engine)
-tables = inspector.get_table_names()
-
-# Print the list of tables
-print("Tables in the database:")
-print(tables)
-
+USING_DB = os.getenv('USING_DB', 'True').lower() == 'true'
+if USING_DB:
+    # Get the database URI from the environment variables
+    URI = os.getenv('DB_URI')
+    DB_URI = URI
+    
+    engine = create_engine(DB_URI)
+    
+    connection = engine.connect()
+    
+    print("DB connection successful!!")
+    
+    inspector = inspect(engine)
+    tables = inspector.get_table_names()
+    
+    # Print the list of tables
+    print("Tables in the database:")
+    print(tables)
+else:
+    tables = []
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -35,9 +37,12 @@ def hello():
 
 @app.route("/tables")
 def list_tables():
-    inspector = inspect(engine)
-    tables = inspector.get_table_names()
-    return "<br>".join(tables)
+    if USING_DB:
+        inspector = inspect(engine)
+        tables = inspector.get_table_names()
+        return "<br>".join(tables)
+    else:
+        return "Database connection is disabled."
 
 if __name__ == "__main__":
     app.run(port="4269")
